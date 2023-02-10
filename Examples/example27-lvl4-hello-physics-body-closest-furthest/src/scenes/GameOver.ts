d2211acaad5190f6acf9af712435c575f1029262
netdebug <francesco.raimondo@gmail.com>
2023-02-10 22:01:27 +0100

    physics closest furthest

---
+++ Examples/example27-lvl4-hello-physics-body-closest-furthest/src/scenes/GameOver.ts
@@ -0,0 +1,18 @@
+export default class GameOver extends Phaser.Scene {
+
+
+  constructor() {
+    super({
+      key: "GameOver",
+    });
+  }
+
+  create() {
+
+    console.log("Create:gameover")
+
+
+  }
+
+
+}
