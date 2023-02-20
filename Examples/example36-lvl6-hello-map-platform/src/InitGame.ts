06b9148feec93faf4494ff7f42c1a385296f1fd2
netdebug <francesco.raimondo@gmail.com>
2023-02-20 18:06:58 +0100

    map2

---
+++ Examples/example36-lvl6-hello-map-platform/src/InitGame.ts
@@ -0,0 +1,51 @@
+import "phaser";
+import Boot from "./scenes/Boot";
+import Preloader from "./scenes/Preloader";
+import Intro from "./scenes/Intro";
+import Hud from "./scenes/Hud";
+import GameOver from "./scenes/GameOver";
+import GamePlay from "./scenes/GamePlay";
+import { GameData } from "./GameData";
+
+window.addEventListener("load", () => {
+
+  const config: any = {
+    type: Phaser.WEBGL,
+    backgroundColor: GameData.globals.bgColor,
+    parent: "my-game",
+    scale: {
+      mode: Phaser.Scale.FIT,
+      width: GameData.globals.gameWidth,
+      height: GameData.globals.gameHeight,
+    },
+
+    scene: [
+      Boot,
+      Preloader,
+      Intro,
+      Hud,
+      GamePlay,
+      GameOver
+    ],
+
+    physics: {
+      default: "arcade",
+      arcade: {
+        debug: GameData.globals.debug,
+        gravity: { y: 600 }
+      }
+    },
+    input: {
+      activePointers: 2,
+      keyboard: true,
+    },
+    render: {
+      pixelArt: true,
+      antialias: false,
+    },
+  };
+
+  const game = new Phaser.Game(config);
+
+
+});
