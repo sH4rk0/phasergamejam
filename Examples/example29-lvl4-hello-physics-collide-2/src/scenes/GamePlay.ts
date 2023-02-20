e419fd64fee4cd38cc749a23c77da2867a81d686
netdebug <francesco.raimondo@gmail.com>
2023-02-20 18:06:22 +0100

    collide-2

---
+++ Examples/example29-lvl4-hello-physics-collide-2/src/scenes/GamePlay.ts
@@ -0,0 +1,16 @@
+
+export default class GamePlay extends Phaser.Scene {
+
+
+  constructor() {
+    super({ key: "GamePlay" });
+  }
+
+  create() {
+    console.log("create:gameplay");
+
+
+  }
+
+  update(time: number, delta: number): void { }
+}
