a0796dec05d0d502c966935e8433e6d0868d4768
netdebug <francesco.raimondo@gmail.com>
2023-02-20 18:06:47 +0100

    map 1

---
+++ Examples/example35-lvl6-hello-map-4-direzioni/src/scenes/Hud.ts
@@ -0,0 +1,14 @@
+
+export default class Hud extends Phaser.Scene {
+
+  constructor() {
+    super({
+      key: "Hud",
+    });
+  }
+
+  create() { }
+
+  update(time: number, delta: number): void { }
+
+}
---
+++ Examples/example35-lvl6-hello-map-4-direzioni/src/scenes/Intro.ts
@@ -0,0 +1,30 @@
+
+
+export default class Intro extends Phaser.Scene {
+
+
+
+
+  constructor() {
+
+    super({ key: "Intro" });
+
+  }
+
+
+  create() {
+
+
+
+
+
+  }
+
+  update(time: number, delta: number): void {
+
+
+
+  }
+
+
+}
